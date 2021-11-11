9d601c288d99a79b584496740123d0e2a7b32de0
kirill <chiril.ghermec@gmail.com>
2021-11-11 12:45:55 +0200

    added fist gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require("gulp");
+const browserSync = require("browser-sync");
+const sass = require("gulp-sass")(require("sass"));
+const rename = require("gulp-rename");
+const autoPrefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+
+gulp.task('server',function(){
+    browserSync.init({
+        server : {
+            baseDir : 'src'
+        }
+    })
+});
+
+gulp.task('styles',function(){
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({
+            outputStyle : 'compressed'
+        }).once('error',sass.logError))
+        .pipe(rename({
+            prefix : "",
+            suffix : ".min"
+        }))
+        .pipe(autoPrefixer({
+            cascade: false
+        }))
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream())
+});
+
+gulp.task('watch',function(){
+    gulp.watch("src/sass/**/*.+(sass|scss)",gulp.parallel('styles'))
+    gulp.watch("src/*.html").on('change',browserSync.reload);
+});
+
+gulp.task('def',gulp.parallel('watch','server','styles'))
